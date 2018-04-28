6c6d67d9083888dcc2142ff9954802dffa55b2bf
Jorge Valdez <jorge@realgeeks.com>
2018-04-28 15:02:52 -0500

    converted to yargs

--- clkn.js
+++ clkn.js
@@ -1,11 +1,13 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+
+const moment = require('moment');
 
 // load env file
 require('dotenv').config();
 
-const program = require('commander');
-program
-  .version('0.1.0')
-  .command('in [when]', 'clock in at a given time', { isDefault: true})
-  .command('out [when]', 'clock out at a given time')
-  .parse(process.argv);
+require('yargs')
+  .version('v0.1.0')
+  .usage('$0 <cmd> [args]')
+  .command(require('./src/commands/in'))
+  .help()
+  .argv;
